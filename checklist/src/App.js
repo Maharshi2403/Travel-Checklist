9777b49b108eacebaab0d0f82edbe02df6a4405a
Maharshi Patel <102695974+Maharshi2403@users.noreply.github.com>
2024-02-19 15:56:04 -0500

    Added some state related functionalities

--- checklist/src/App.js
+++ checklist/src/App.js
@@ -1,5 +1,5 @@
 import "./index.css";
-
+import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,16 +20,42 @@ export default function App() {
 function Logo() {
   return <h1>✈️Far Away👜</h1>;
 }
+
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const [id, setId] = useState(4);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newData = {
+      id: id,
+      description: description,
+      quantity: quantity,
+      package: false,
+    };
+    setId(id + 1);
+    console.log(newData);
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need to pack 😁 for your trip.</h3>
-      <select>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num   ) => (
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num}>{num}</option>
         ))}
       </select>
-      <input type="text" placeholder="Item..."></input>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
       <button>Add</button>
     </form>
   );
@@ -39,7 +65,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item itemObj={item} />
+          <Item itemObj={item} key={item.id} />
         ))}
       </ul>
     </div>
